dc60894ca1fa5e0d6f897ea770f23ac68ba66978
Helen CG <helencochchainguillemro.04@gmail.com>
2024-11-21 12:44:57 -0500

    funcion

--- MostrarResultados.js
+++ MostrarResultados.js
@@ -1,1 +1,8 @@
-
+function mostrarResultado(valorConvertido) {
+    const resultDiv = document.getElementById("result");
+    if (resultDiv) {
+        resultDiv.textContent = `El resultado de la conversión es: ${valorConvertido}`;
+    } else {
+        console.error("El elemento con id 'result' no existe en el documento HTML.");
+    }
+}
